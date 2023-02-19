e4611a85989c7319d90b577fa83ca493bb56e39d
FoxtrotPapa_25 <Francois25@users.noreply.github.com>
2023-02-19 02:14:43 +0100

    data_analyse vérifié

--- data_analyse.py
+++ data_analyse.py
@@ -1,4 +1,3 @@
-# from datetime import datetime
 from utime import localtime
 import requests
 import wificonfig
@@ -7,15 +6,27 @@ def send_data(volume_value):
     api_key = wificonfig.api_key
 
     date = localtime()
-    mois = str(localtime().tm_mon).zfill(2)
-    date = f"{localtime().tm_mday}.{mois}.{localtime().tm_year} {localtime().tm_hour}:{localtime().tm_min}:{localtime().tm_sec}"
-
+    mois = str(localtime()[1])#.zfill(2)
+    heure = str(localtime()[3])
+    minute = str(localtime()[4])
+    seconde = str(localtime()[5])
+    if len(mois) == 1:
+        mois = "0" + mois
+    if len(heure) == 1:
+        heure = "0" + heure
+    if len(minute) == 1:
+        minute = "0" + minute
+    if len(seconde) == 1:
+        seconde = "0" + seconde
+    date = f"{localtime()[2]}.{mois}.{localtime()[0]}%{heure}:{minute}:{seconde}"
+    
     niveau = "?date="+ date + "&niveau="+ str(volume_value)
     try:
         request_headers = {'Content-Type': 'application/x-www-form-urlencoded' }
         url = f'https://script.google.com/macros/s/{api_key}/exec{niveau}'
         request = requests.get(url, headers=request_headers)
         request.close()
+        print("Data saved")
 
     except OSError as e:
         print('Failed to read/publish sensor readings.')
